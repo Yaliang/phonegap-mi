76d86a696284f4bc3f007c74f725c7b6576cf3e5
Yaliang Wang <yaliangwang2015@u.northwestern.edu>
2015-04-24 17:16:21 -0500

    unable to fix it

--- www/script/functions.js
+++ www/script/functions.js
@@ -520,13 +520,10 @@ function sendToolbarActiveKeyboard(object){
     $("html body").animate({ scrollTop: $(document).height().toString()+"px" }, {
         duration: 300,
         complete : function(){
-            $(object.id).prop("disabled", false);
             $(object.bar).css("position","absolute");
             $(object.bar).css("bottom",($("body").height() - $(object.base).height()+object.bias).toString()+"px");
+            $(object.id).prop("disabled", false);
             $(object.id).trigger("focus");
-            if (!cordova.plugins.Keyboard.isVisible) {
-                sendToolbarActiveKeyboard(object);
-            }
         }
     });
 }
