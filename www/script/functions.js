d9ff7284f913fabe7e4a2da91fc5ec0a87fe7841
Yaliang Wang <yaliangwang2015@u.northwestern.edu>
2015-04-25 18:29:31 -0500

    get information

--- www/script/functions.js
+++ www/script/functions.js
@@ -161,6 +161,18 @@ function initialElementEventSetting(){
         $("#body-input-old-password").focus();
     });
 
+    var txt = "";
+    txt += "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
+    txt += "<p>Browser Name: " + navigator.appName + "</p>";
+    txt += "<p>Browser Version: " + navigator.appVersion + "</p>";
+    txt += "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>";
+    txt += "<p>Browser Language: " + navigator.language + "</p>";
+    txt += "<p>Browser Online: " + navigator.onLine + "</p>";
+    txt += "<p>Platform: " + navigator.platform + "</p>";
+    txt += "<p>User-agent header: " + navigator.userAgent + "</p>";
+
+    alert(txt);
+
 }
 
 /* This function tries to log into user session by local storage
